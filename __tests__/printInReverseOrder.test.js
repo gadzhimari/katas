94b03b09565bf06b3af43a5ae706bc5321144120
Gadzhimari Kachaev <gadzhimari@gmail.com>
2017-06-15 17:17:02 +0300

    Add solution of print digits in reverse order problem

---
+++ __tests__/printInReverseOrder.test.js
@@ -0,0 +1,8 @@
+import printInReverseOrder from '../src/printInReverseOrder/printInReverseOrder';
+
+test('printInReverseOrder', () => {
+  expect(printInReverseOrder(17)).toBe('7 1');
+  expect(printInReverseOrder(211)).toBe('1 1 2');
+  expect(printInReverseOrder(4317)).toBe('7 1 3 4');
+  expect(printInReverseOrder(51239991)).toBe('1 9 9 9 3 2 1 5');
+});
---
+++ src/printInReverseOrder/printInReverseOrder.js
@@ -0,0 +1,6 @@
+export default (num) => {
+  const iter = (restNum, acc) => 
+    (restNum === 0) ? acc.slice(0, -1) : iter(~~(restNum / 10), `${acc}${restNum % 10} `); 
+
+  return iter(num, ''); 
+}
