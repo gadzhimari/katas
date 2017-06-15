75716c07e59fd09075e45d93678da21c0ac74444
Gadzhimari Kachaev <gadzhimari@gmail.com>
2017-06-15 15:50:57 +0300

    Add print in reverse order problem

---
+++ __tests__/printInReverseOrder.test.js
@@ -0,0 +1,7 @@
+import printInReverseOrder from './printInReverseOrder/printInReverseOrder';
+
+test('printInReverseOrder', () => {
+  expect(printInReverseOrder(211)).toBe('1 1 2');
+  expect(printInReverseOrder(4317)).toBe('7 1 3 4');
+  expect(printInReverseOrder(51239991)).toBe('1 9 9 9 3 2 1 5');
+});
