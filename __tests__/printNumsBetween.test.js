05e2bfdb35e466f55aff03fc65e5aebecb6c0135
Gadzhimari Kachaev <gadzhimari@gmail.com>
2017-06-20 16:27:45 +0300

    Add solution to print nums between problem

---
+++ __tests__/printNumsBetween.test.js
@@ -0,0 +1,6 @@
+import printNumsBetween from '../src/printNumsBetween/printNumsBetween';
+
+test('printNumsBetween', () => {
+  expect(printNumsBetween(10, 15)).toBe('10 11 12 13 14 15');
+  expect(printNumsBetween(15, 10)).toBe('15 14 13 12 11 10');
+})
---
+++ src/printNumsBetween/printNumsBetween.js
@@ -0,0 +1,11 @@
+const printNumsBetween = (a, b) => {
+  if (a < b) {
+    return a + ' ' + printNumsBetween(a + 1, b);
+  } else if (a > b) {
+    return a + ' ' + printNumsBetween(a - 1, b);
+  }
+
+  return a;
+}
+
+export default printNumsBetween;
