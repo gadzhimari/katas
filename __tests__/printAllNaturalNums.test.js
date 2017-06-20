3048b34964edd485a630350a9e3174546d072240
Gadzhimari Kachaev <gadzhimari@gmail.com>
2017-06-20 16:04:24 +0300

    Fix test in print all natural nums problem

--- __tests__/printAllNaturalNums.test.js
+++ __tests__/printAllNaturalNums.test.js
@@ -1,7 +1,7 @@
 import printAllNaturalNums from '../src/printAllNaturalNums/printAllNaturalNums';
 
 test('printAllNaturalNums', () => {
-  expect(printAllNaturalNums(2)).toBe('1, 2');
-  expect(printAllNaturalNums(7)).toBe('1, 2, 3, 4, 5, 6, 7');
-  expect(printAllNaturalNums(10)).toBe('1, 2, 3, 4, 5, 6, 7, 8, 9, 10');
+  expect(printAllNaturalNums(2)).toBe('1 2');
+  expect(printAllNaturalNums(7)).toBe('1 2 3 4 5 6 7');
+  expect(printAllNaturalNums(10)).toBe('1 2 3 4 5 6 7 8 9 10');
 });
--- src/printAllNaturalNums/README.md
+++ src/printAllNaturalNums/README.md
@@ -1,4 +1,4 @@
 Print all natural numbers from 1 to n using recursion.
 ```js
-printAllNaturalNums(10); // Output "1, 2, 3, 4, 5, 6, 7, 8, 9, 10"
+printAllNaturalNums(10); // Output "1 2 3 4 5 6 7 8 9 10"
 ```
