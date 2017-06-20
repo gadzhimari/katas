47d400d693bf496d1200a8531242d198f2d09d3f
Gadzhimari Kachaev <gadzhimari@gmail.com>
2017-06-20 15:16:39 +0300

    Delete count sum unique digit problem

--- __tests__/countSumUniqueDigit.test.js
+++
@@ -1,7 +0,0 @@
-import countSumUniqueDigit from '../src/countSumUniqueDigit/countSumUniqueDigit';
-
-test('countSumUniqueDigit', () => {
-  expect(countSumUniqueDigit(3, 2)).toBe(0);
-  expect(countSumUniqueDigit(3, 3)).toBe(1);
-  expect(countSumUniqueDigit(3, 6)).toBe(7);
-});
