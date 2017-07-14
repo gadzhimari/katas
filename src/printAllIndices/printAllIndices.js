8544b28e728e012dfa055c5e7b2f3e3982336925
Gadzhimari Kachaev <gadzhimari@gmail.com>
2017-07-14 15:56:16 +0300

    Add solution of print indices problem

---
+++ src/printAllIndices/printAllIndices.js
@@ -0,0 +1,14 @@
+export default (arr) => {
+  if (arr.length === 0) {
+    return [];
+  }
+  const sortedArr = arr.reduce((acc, str, index) => {
+    const newStr = str.split('').sort().join('');
+
+    return acc.has(newStr) ?
+      acc.set(newStr, [...acc.get(newStr), index]) : acc.set(newStr, [index]);
+  }, new Map());
+
+  return [...sortedArr.values()].reduce((acc, indices) =>
+    indices.length > 1 ? [...acc, indices] : acc, []);
+}
