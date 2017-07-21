4984ab440b2ac51f8f296ce1ec70eab6b3a6579b
Gadzhimari Kachaev <gadzhimari@gmail.com>
2017-07-21 14:44:01 +0300

    Rename mergeSort function

--- __tests__/mergeSort.test.js
+++
@@ -1,7 +0,0 @@
-import mergeSort from '../src/mergeSort/mergeSort';
-
-test('mergeSort', () => {
-  expect(mergeSort([], [])).toEqual([]);
-  expect(mergeSort([1, 2], [])).toEqual([1, 2]);
-  expect(mergeSort([1, 5, 10], [5, 6, 15, 100])).toEqual([1, 5, 5, 6, 10, 15, 100]);
-})
