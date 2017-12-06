369783374976244181cb42bbb7252c6aee48a879
Gadzhimari Kachaev <gadzhimari@gmail.com>
2017-12-06 13:23:38 +0300

    Add solution of sort deps problem. Update dependencies. Add stage-0 in package.json

---
+++ src/sortDeps/sortDeps.js
@@ -0,0 +1,12 @@
+const sortDeps = (deps) => {
+  const add = (acc, node) => {
+    const subDeps = deps[node];
+    const subAcc = subDeps ? subDeps.reduce(add, []) : [];
+    return { ...acc, ...subAcc, [node]: true };
+  };
+
+  const set = Object.keys(deps).reduce(add, []);
+  return Object.keys(set);
+};
+
+export default sortDeps;
